3d03cfe19d2d2484f2f6f7b52e7b704d0c6aaf7a
qinchanggui <qinchangling@163.com>
2020-05-30 21:20:52 +0800

    Initial commit

---
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ssm.service.impl;
+
+import com.ssm.dao.IUserDao;
+import com.ssm.model.User;
+import com.ssm.service.IUserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+
+    @Resource
+    private IUserDao userDao;
+
+    public User selectUser(long userId) {
+        return userDao.selectUser(userId);
+    }
+}
